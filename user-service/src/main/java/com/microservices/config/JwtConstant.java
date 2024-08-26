35fadcce2558ec5bd7fddaed347d72bce8e1dcd1
vishaldange <vishaldange2002@gmail.com>
2024-08-26 22:45:51 +0530

    Basic setup of project and dependencies

---
+++ user-service/src/main/java/com/microservices/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.microservices.config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY="ghdlslseskljfgekyuvhjbakhnvbgarkbhjh";
+
+    public static final String JWT_HEADER= "Authorization";
+}
