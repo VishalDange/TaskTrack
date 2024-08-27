0e702665f9d279d793ce030200757a98456e2549
vishaldange <vishaldange2002@gmail.com>
2024-08-27 22:44:01 +0530

    Update JwtProvider.java

--- user-service/src/main/java/com/microservices/config/JwtProvider.java
+++ user-service/src/main/java/com/microservices/config/JwtProvider.java
@@ -26,36 +26,15 @@ public class JwtProvider extends OncePerRequestFilter {
 
     }
 
-    public String generateToken(Authentication auth){
-
-        Collection<? extends GrantedAuthority> authorities=auth.getAuthorities();
-        String roles=populateAuthorities(authorities);
-
-        String jwt= Jwts.builder().setIssuedAt(new Date()).setExpiration(new Date(new Date().getTime()+86400000)).claim("email",auth.getName()).claim("authorities",roles).signWith(key).compact();
-
-        return jwt;
-    }
-
-    public String getEmailFromJwtToken(String jwt){
-        jwt=jwt.substring(7);
-        SecretKey key= Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
-
-        Claims claims= Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
-
-        String email=String.valueOf(claims.get("email"));
-
-        return email;
-    }
-
-    private String populateAuthorities(Collection<? extends GrantedAuthority> authorities) {
-
-        Set<String> auths=new HashSet<>();
-        for(GrantedAuthority authority:authorities){
-            auths.add(authority.getAuthority());
-        }
-
-        return String.join(",",auths);
-    }
+//    public String generateToken(Authentication auth){
+//
+//        Collection<? extends GrantedAuthority> authorities=auth.getAuthorities();
+//        String roles=populateAuthorities(authorities);
+//
+//        String jwt= Jwts.builder().setIssuedAt(new Date()).setExpiration(new Date(new Date().getTime()+86400000)).claim("email",auth.getName()).claim("authorities",roles).signWith(key).compact();
+//
+//        return jwt;
+//    }
 
 
 }
