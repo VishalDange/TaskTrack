c3755105dcbb2fde47d51efb542c871f151b9ed5
vishaldange <vishaldange2002@gmail.com>
2024-10-03 11:34:39 +0530

    Design the Sidebar

--- frontend/src/Page/Sidebar/Sidebar.jsx
+++ frontend/src/Page/Sidebar/Sidebar.jsx
@@ -1,9 +1,30 @@
-import { Avatar } from "@mui/material";
-import React from "react";
+import { Avatar, Button } from "@mui/material";
+import React, { useState } from "react";
+import "./Sidebar.css";
+
+const menu = [
+  { name: "Home", value: "HOME", role: ["ROLE_ADMIN", "ROLE_CUSTOMER"] },
+  { name: "DONE", value: "DONE", role: ["ROLE_ADMIN", "ROLE_CUSTOMER"] },
+  { name: "ASSIGNED", value: "ASSIGNED", role: ["ROLE_ADMIN"] },
+  { name: "NOT ASSIGNED", value: "PENDING", role: ["ROLE_ADMIN"] },
+  { name: "Create New Task", value: "", role: ["ROLE_ADMIN"] },
+  { name: "Notification", value: "NOTIFICATION", role: ["ROLE_CUSTOMER"] },
+];
+
+const role = "ROLE_ADMIN";
 
 const Sidebar = () => {
+  const [activeMenu, setActiveMenu] = useState("Home");
+  const handleMenuChange = (item) => {
+    setActiveMenu(item.name);
+  };
+
+  const handleLogout = () => {
+    console.log("handle Logout..");
+  };
+
   return (
-    <div className="min-h-[85vh] flex flex-col justify-center fixed w-[20vw]">
+    <div className=" card min-h-[85vh] flex flex-col justify-center fixed w-[20vw]">
       <div className="space-y-5 h-full">
         <div className="flex justify-center">
           <Avatar
@@ -13,6 +34,28 @@ const Sidebar = () => {
             alt="company_logo"
           />
         </div>
+
+        {menu
+          .filter((item) => item.role.includes(role))
+          .map((item) => (
+            <p
+              onClick={() => handleMenuChange(item)}
+              className={`py-3 px-5 rounded-full text-center cursor-pointer ${
+                activeMenu === item.name ? "activeMenuItem" : "menuItem"
+              }`}
+            >
+              {item.name}
+            </p>
+          ))}
+
+        <Button
+          onClick={handleLogout}
+          fullWidth
+          sx={{ padding: ".7rem", borderRadius: "2rem" }}
+          className="logoutButton"
+        >
+          Logout
+        </Button>
       </div>
     </div>
   );
